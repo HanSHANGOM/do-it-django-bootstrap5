340be8e9d4e35f467db1c8cea5a8a4c0673b5dd0
hanshangom <zxcvb0294@gmail.com>
2022-01-23 18:59:11 +0900

    포스트 목록 페이지에 대표 이미지 출력하기

--- django_proj/blog/urls.py
+++ django_proj/blog/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from . import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('/<int:pk>',views.PostDetail.as_view()),
     path('/<int:pk>/',views.PostDetail.as_view()),
     #path('',views.index),
     path('',views.PostList.as_view())
 ]
+
--- django_proj/do_it_django/urls.py
+++ django_proj/do_it_django/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('blog',include('blog.urls')),
@@ -24,3 +25,4 @@ urlpatterns = [
     path('',include('single_pages.urls')),
     
 ]
+urlpatterns.extend(static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
