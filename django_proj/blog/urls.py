d0bd685dafb15be9cf28ccbe72defed0b85f0542
hanshangom <zxcvb0294@gmail.com>
2022-02-20 20:41:40 +0900

    카테고리 페이지 만들기

--- django_proj/blog/urls.py
+++ django_proj/blog/urls.py
@@ -5,6 +5,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('/category/<str:slug>/', views.category_page),
+    path('/category/<str:slug>', views.category_page),
     path('/<int:pk>',views.PostDetail.as_view()),
     path('/<int:pk>/',views.PostDetail.as_view()),
     #path('',views.index),
